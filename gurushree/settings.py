368bfc449cf990ba3ca36cfaca4b4d79faeefd62
Samiran Sarkar <samiransarkar513@gmail.com>
2021-01-11 15:29:30 +0530

    sd

--- gurushree/settings.py
+++ gurushree/settings.py
@@ -143,9 +143,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
+WHITENOISE_USE_FINDERS = True
 
 
 
